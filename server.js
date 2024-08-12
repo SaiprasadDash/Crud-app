680c1faf33dd8d56004abcc8d028185f12ba2f0c
SaiprasadDash <dashsaiprasad831@gmail.com>
2024-08-13 01:16:51 +0530

    part-1

--- server.js
+++ server.js
@@ -16,12 +16,20 @@ app.use(bodyParser.urlencoded({extended : true}))
 app.set("view engine", "ejs")
 // app.set("views", path.resolve(__dirname, ""))
 
-app.set("/css", express.static(path.resolve(__dirname, "assets/css")))
-app.set("/js", express.static(path.resolve(__dirname, "assets/js")))
-app.set("/img", express.static(path.resolve(__dirname, "assets/img")))
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")))
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")))
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")))
 
 app.get('/', (req, res) =>{
     res.render('index.ejs');
+});
+
+app.get('/add-user', (req, res) =>{
+    res.render('add_user.ejs');
+});
+
+app.get('/update-user', (req, res) =>{
+    res.render('update_user.ejs');
 });
 
 app.listen(PORT, ()=> {
